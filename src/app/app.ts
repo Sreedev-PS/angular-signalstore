6ab682b43a2f00166afafcd75e6cc720db351e0e
Sreedev-PS <sreedev.p.skn@gmail.com>
2026-07-15 21:55:51 +0530

    cc

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,22 @@
-import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { signalStore, withState } from '@ngrx/signals';
+  import { signal } from '@angular/core';
 
-type Lesson = { eyebrow: string; title: string; concept: string; code: string };
+
+const CountStore = signalStore(
+  withState({ count: 0 })
+);
 
 @Component({
   selector: 'app-root', 
   imports: [FormsModule], 
+  providers: [CountStore],
   templateUrl: './app.html', 
   styleUrl: './app.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class App { 
+
+private count = signal(0);
 }
